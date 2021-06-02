27ab1d20a500b3bcf091e90f6ef84f4d75e2d4c7
Anupam <anupam.saini18@gmail.com>
2021-06-02 23:02:04 +0530

    Forth commit

--- app.py
+++ app.py
@@ -218,6 +218,6 @@ def predict():
     return render_template('predict.html')
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run(threaded=True, port=5000, debug=True)
     
 
